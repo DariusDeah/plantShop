ce97d7325ce2713cf8be77aacbfda68dde5d7986
darius <dariusdeah21@gmail.com>
2021-10-12 23:17:51 -0600

    item schema completed

--- plantShop/server/models/Item.js
+++ plantShop/server/models/Item.js
@@ -1,1 +1,11 @@
 import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const ItemSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  category: { enum: ['NEW', 'Popular', 'Indoor', 'Outdoor'] },
+  img: { type: String, required: true },
+  details: { type: String, required: true }
+})
